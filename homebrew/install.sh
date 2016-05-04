c3c098f08ce7013bd87aae6e11928ebdc0ed5ba5
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-05-04 17:45:42 +1200

    Clean things up and begin merging in my existing settings

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,20 +5,25 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew)
+if test "$(uname)" = "Darwin"
 then
-  echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
-  fi
+	# Install Homebrew if necessary
+	if test ! $(which brew)
+	then
+		echo "> Installing Homebrew"
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
 
+	# Update Homebrew
+	echo "› brew update"
+	brew update
+
+	( # Run the Brewfile through Homebrew
+		echo "› brew bundle"
+		cd "$(dirname $0)"/..
+		brew bundle
+	)
 fi
 
 exit 0
