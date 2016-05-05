14b552184ce703a68439a55ac29308bdef53f2b5
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-05-06 10:09:25 +1200

    Override fg and bg to add bash-compatible syntax

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -7,3 +7,19 @@ SAVEHIST=10000
 
 # Default user for theme
 DEFAULT_USER=Matthew
+
+# Override builtin fg and bg to add bash compatibility (i.e. fg 1 instead of fg %1)
+fg() {
+	if [[ "$*" =~ ^[0-9]+$ ]]; then 
+		builtin fg %"$*"
+	else 
+		builtin fg "$@"
+	fi 
+}
+bg() {
+	if [[ "$*" =~ ^[0-9]+$ ]]; then 
+		builtin bg %"$*"
+	else 
+		builtin bg "$@"
+	fi 
+}
