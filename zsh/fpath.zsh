2415e436f5dc259cf9c8496cff7fa6c5fa3a8b6f
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-05-17 14:35:15 +1200

    Use $DOTFILES instead of $ZSH to avoid clash with omz, only add functions/ to $FPATH

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,3 @@
-#add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+# Add functions/ to fpath and autoload all files in it
+fpath=($DOTFILES/functions $fpath)
+autoload -U $DOTFILES/functions/*(:t)
