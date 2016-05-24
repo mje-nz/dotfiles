821e97644b53735bf5c5018f3979f98509bb8a1c
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-05-24 16:57:00 +1200

    Don't brew update etc every time setup is called

--- osx-only/homebrew/install.sh
+++ osx-only/homebrew/install.sh
@@ -12,15 +12,15 @@ then
 	if test ! $(which brew); then
 		echo "> Installing Homebrew"
 		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+		# Update Homebrew
+		echo "› brew update"
+		brew update
+
+		# Run the Brewfile through Homebrew
+		echo "› brew bundle"
+		pushd "$(dirname $0)"
+		brew bundle
+		popd
 	fi
-
-	# Update Homebrew
-	echo "› brew update"
-	brew update
-
-	# Run the Brewfile through Homebrew
-	echo "› brew bundle"
-	pushd "$(dirname $0)"
-	brew bundle
-	popd
 fi
