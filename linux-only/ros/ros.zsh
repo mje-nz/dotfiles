df823f7c7c794d249a7b1446199375fb92bc8bf0
Matthew Edwards <matthew.edwards@pg.canterbury.ac.nz>
2016-05-28 16:24:17 +1200

    Add alias for overlaying catkin devel space

--- linux-only/ros/ros.zsh
+++ linux-only/ros/ros.zsh
@@ -7,9 +7,17 @@ if [[ -d /opt/ros/indigo/ ]]; then
   # My aliases
   alias iv='rosrun image_view image_view'
   alias iva='rosrun image_view image_view _autosize:=true'
-
+  
+  # Set ROS_MASTER_URI
   rosmaster() {
     local host=$1
     export ROS_MASTER_URI="http://$1:11311"
+  }
+  
+  # Source catkin_ws/(space)/setup.zsh
+  cs() {
+    local space=${1:=devel}
+    echo "Overlaying $1 workspace"
+    source "$1/setup.zsh"
   }
 fi
