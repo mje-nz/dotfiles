bbce86a1d4834e5eec7edfb1a0c0858a4f73a703
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-07-15 14:30:19 +1200

    Fix gdt alias

--- git/aliases.2.zsh
+++ git/aliases.2.zsh
@@ -1,3 +1,5 @@
+# Must run after zgen unaliases gdt
+
 # holman's git aliases
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
