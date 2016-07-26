13ede3ec7bcee8734aba64656f1085afa8c03e51
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-07-26 13:37:19 +1200

    Detect ROS Kinetic

--- linux-only/ros/ros.zsh
+++ linux-only/ros/ros.zsh
@@ -1,9 +1,19 @@
+#!/usr/bin/env zsh
+
 # ROS
 # If ROS is installed, this loads its setup script and does some tweaks
 
+ros_found=
+
 if [[ -d /opt/ros/indigo/ ]]; then
   source /opt/ros/indigo/setup.zsh
-  
+  ros_found=true
+elif [[ -d /opt/ros/kinetic/ ]]; then
+  source /opt/ros/kinetic/setup.zsh
+  ros_found=true
+fi
+
+if [[ $ros_found == "true" ]]; then
   # My aliases
   alias iv='rosrun image_view image_view'
   alias iva='rosrun image_view image_view _autosize:=true'
