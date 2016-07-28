4dc980d3577f1d2e044ef5474cfb4968ec95e7c2
Matthew Edwards <matthew@matthewedwards.co.nz>
2016-07-28 15:20:20 +1200

    Add more keyboard shortcuts

---
+++ linux-only/mint-keybindings/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Set OSX-like keybindings for Mint (and probably Gnome in general)
+
+
+gsettings_array_add() {
+	local path=$1 key=$2 new_value=$3
+	local orig_value="$(gsettings get $path $key)"
+	
+	# Test if new_value is already in orig_value
+	if [[ "$orig_value" == *"$new_value"* ]]; then
+		return
+	fi
+	
+	echo "Adding keyboard shortcut for $key"
+	# Create array or append new_value
+	if [[ "$orig_value" == "@as []" ]]; then
+		gsettings set "$path" "$key" "['$new_value']"
+	else
+		gsettings set "$path" "$key" "$(echo $orig_value | sed -e "s/]$/, '$new_value']/")"
+	fi
+}
+
+
+# Add bindings
+gsettings_array_add org.cinnamon.desktop.keybindings.wm close "<Super>Q"
+gsettings_array_add org.cinnamon.desktop.keybindings.wm switch-windows "<Super>Tab"
+gsettings_array_add org.cinnamon.desktop.keybindings.wm switch-windows-backward "<Shift><Super>Tab"
+gsettings_array_add org.cinnamon.desktop.keybindings.wm switch-group "<Super>grave"
+gsettings_array_add org.cinnamon.desktop.keybindings.wm switch-group-backward "<Shift><Super>asciitilde"
+gsettings_array_add org.cinnamon.desktop.keybindings.wm panel-run-dialog "<Super>space"
+gsettings set org.cinnamon.desktop.keybindings looking-glass-keybinding "[]"
+gsettings_array_add org.cinnamon.desktop.keybindings.media-keys screensaver "<Super>l"
+
