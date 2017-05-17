d71da74d1f0c24514525638a9f149e9dfd873705
Matthew Edwards <matthew@matthewedwards.co.nz>
2017-05-17 15:41:29 +1200

    Add built sympytex

---
+++ thirdparty/sympytex.zsh
@@ -0,0 +1,3 @@
+# Add symptex folder to Python and TeX paths
+export PYTHONPATH=$PYTHONPATH:$DOTFILES/thirdparty/sympytex
+export TEXINPUTS=$TEXINPUTS:$DOTFILES/thirdparty/sympytex:
---
+++ thirdparty/sympytex/README.md
@@ -0,0 +1,1 @@
+These files are built from master 17524c8, see https://github.com/tmolteno/SympyTeX
