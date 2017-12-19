d1b9f65dce26f5613079fc5b13cc206eb76f2797
Matthew Edwards <mje-nz@users.noreply.github.com>
2017-12-20 12:35:52 +1300

    Add fzf

--- thirdparty/install.sh
+++ thirdparty/install.sh
@@ -3,3 +3,7 @@
 if [ ! -d thirdparty/zsh-autosuggestions.noexec ]; then
 	git clone --recursive git://github.com/zsh-users/zsh-autosuggestions thirdparty/zsh-autosuggestions.noexec
 fi
+
+# Run fzf installer and move generated rc file
+thirdparty/fzf.noexec/install --key-bindings --completion --no-update-rc
+mv ~/.fzf.zsh thirdparty/
