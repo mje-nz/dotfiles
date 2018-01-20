8340f2875d5421d66b3743ea020253f55c9372d6
Matthew Edwards <mje-nz@users.noreply.github.com>
2018-01-20 16:54:49 +1300

    Add caps2esc install script on Linux

--- linux-only/install.sh
+++ linux-only/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source $DOTFILES/setup_common.sh
+set -e
 
 yesno() {
 	local message=$1
@@ -36,3 +37,50 @@ if [ "$SHELL" != $(which zsh) ]; then
 	fi
 fi
 
+if yesno "Install caps2esc (will use sudo)?"; then
+	echo "Installing dependencies"
+	sudo apt-get install build-essential cmake libevdev-dev libyaml-cpp-dev
+	echo "Building and installing interception tools"
+	pushd $(mktemp -d) > /dev/null
+	echo "(in temp directory $(pwd)"
+	git clone https://gitlab.com/interception/linux/tools.git
+	cd tools
+	mkdir build
+	cd build
+	cmake ..
+	make
+	sudo make install
+	popd > /dev/null
+	echo "Building and installing caps2esc"
+	pushd $(mktemp -d) > /dev/null
+	echo "(in temp directory $(pwd)"
+	git clone https://gitlab.com/interception/linux/plugins/caps2esc.git
+	cd caps2esc
+	git apply $DOTFILES/linux-only/caps2esc.patch
+	mkdir build
+	cd build
+	cmake ..
+	make
+	sudo make install
+	popd > /dev/null
+	echo "Creating and starting udevmon service"
+	sudo tee "/etc/udevmon.yaml" > /dev/null <<EOF
+- JOB: "intercept -g \$DEVNODE | caps2esc | uinput -d \$DEVNODE"
+  DEVICE:
+    EVENTS:
+      EV_KEY: [KEY_CAPSLOCK]
+EOF
+	sudo tee "/etc/systemd/system/udevmon.service" > /dev/null <<EOF
+[Unit]
+Description=udevmon
+After=systemd-user-sessions.service
+
+[Service]
+ExecStart=/usr/bin/nice -n -20 /usr/local/bin/udevmon -c /etc/udevmon.yaml
+
+[Install]
+WantedBy=multi-user.target
+EOF
+	sudo systemctl enable udevmon
+	success "Installed caps2esc"
+fi
