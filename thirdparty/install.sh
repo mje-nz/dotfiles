24bbe6f4981ba5c7154c158a331168f249d290d4
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-01-10 14:56:21 +1300

    Add messages to thirdparty installer

--- thirdparty/install.sh
+++ thirdparty/install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+
 # Clone zsh-autosuggestions -- it uses submodules so I can't add it as a subrepo
 if [ ! -d thirdparty/zsh-autosuggestions.noexec ]; then
+	echo "Checking out zsh-autosuggestions"
 	git clone --recursive git://github.com/zsh-users/zsh-autosuggestions thirdparty/zsh-autosuggestions.noexec
 fi
 
 # Run fzf installer and move generated rc file
 if [ ! -f thirdparty/fzf.zsh ]; then
+	echo "Setting up fzf"
 	thirdparty/fzf.noexec/install --key-bindings --completion --no-update-rc
-	mv ~/.fzf.zsh thirdparty/fzf.zsh
-	mv ~/.fzf.bash thirdparty/fzf.bash
+	mv ~/.fzf.zsh $DOTFILES/thirdparty/fzf.zsh
+	mv ~/.fzf.bash $DOTFILES/thirdparty/fzf.bash
 fi
