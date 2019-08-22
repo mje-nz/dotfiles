110b3e48f0192ca9c75cc5a608457abb73e7edcd
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-08-22 15:06:07 +1200

    Only remap caps lock to escape (not actual control key), document Hammerspoon better

---
+++ osx-only/hammerspoon.symlink/Readme.md
@@ -0,0 +1,8 @@
+# Hammerspoon config
+
+* Remap Caps Lock to Control (when held) and Escape (when pressed)
+* Switch to and from the network and audio adaptors on my Thunderbolt dock automatically
+* Mute speakers when waking from sleep
+* Mute speakers when leaving home WiFi
+
+The keyboard remapping requires Hammerspoon to be enabled in `System Preferences > Security & Privacy > Privacy > Accessibility`, and Caps Lock to be mapped to Control in `System Preferences > Keyboard > Keyboard > Modifier Keys`.
--- osx-only/hammerspoon.symlink/control-escape.lua
+++ osx-only/hammerspoon.symlink/control-escape.lua
@@ -1,7 +1,9 @@
+-- Map a short press of the caps lock key to escape.
+-- To use this, map caps lock to control in System Preferences.
 -- From https://github.com/jasonrudolph/keyboard/blob/master/hammerspoon/control-escape.lua
 
 sendEscape = false
-lastMods = {}
+lastFlags = {}
 
 ctrlKeyHandler = function()
   sendEscape = false
@@ -9,33 +11,40 @@ end
 
 ctrlKeyTimer = hs.timer.delayed.new(0.15, ctrlKeyHandler)
 
-ctrlHandler = function(evt)
-  local newMods = evt:getFlags()
-  if lastMods["ctrl"] == newMods["ctrl"] then
+flagsChangedHandler = function(evt)
+  if evt:getKeyCode() ~= 62 then
+    -- Only modify caps lock events
     return false
   end
-  if not lastMods["ctrl"] then
-    lastMods = newMods
+  local newFlags = evt:getFlags()
+  if lastFlags.ctrl == newFlags.ctrl then
+    return false
+  end
+  if not lastFlags.ctrl then
+    -- Control has just been pressed
+    lastFlags = newFlags
     sendEscape = true
     ctrlKeyTimer:start()
   else
+    -- Control has just been released
     if sendEscape then
+      print('Sending escape')
       keyUpDown({}, 'escape')
     end
-    lastMods = newMods
+    lastFlags = newFlags
     ctrlKeyTimer:stop()
   end
   return false
 end
 
-ctrlTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, ctrlHandler)
-ctrlTap:start()
+flagsTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, flagsChangedHandler)
+flagsTap:start()
 
-otherHandler = function(evt)
+keyDownHandler = function(evt)
   sendEscape = false
   return false
 end
 
-otherTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, otherHandler)
-otherTap:start()
+keyDownTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, keyDownHandler)
+keyDownTap:start()
 
