a3bbe3c13f887ff69c3460633c82bd942ba7d02c
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-08-25 09:47:08 +1200

    Move misc/aliases.zsh into commonrc, add some aliases from oh-my-zsh/common-aliases

--- zsh/general.zsh
+++ zsh/general.zsh
@@ -21,3 +21,14 @@ bg() {
 		builtin bg "$@"
 	fi 
 }
+
+# Global aliases from oh-my-zsh/common-aliases
+# e.g. "env G PATH" expands to "env | grep PATH"
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+alias -g M="| more"
+alias -g LL="2>&1 | less"
+alias -g N="2> /dev/null"
+alias -g NN="> /dev/null 2>&1"
