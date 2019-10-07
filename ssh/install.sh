d40a21407383be852f972eb42037119cc1137f69
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-10-08 10:20:09 +1300

    Add ssh config

--- ssh/install.sh
+++ ssh/install.sh
@@ -5,14 +5,12 @@ set -e
 # shellcheck disable=SC1090
 source "$DOTFILES/setup_common.sh"
 
-
-link_scripts() {
+link_config() {
 	# TODO is this right?
 	# shellcheck disable=SC2034
 	local overwrite_all=false backup_all=false skip_all=false
-	mkdir -p ~/.config/autokey/data
-	link_file "$DOTFILES/linux-only/autokey/osxkeys/" "$HOME/.config/autokey/data/OSXKeys"
+	mkdir -p ~/.ssh
+	link_file "$DOTFILES/ssh/config" "$HOME/.ssh/config"
 }
 
-link_scripts
-
+link_config
