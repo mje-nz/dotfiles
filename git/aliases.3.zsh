244c12370962324c1f7d64cda4b8bcc80fd9ce86
Matthew Edwards <mje-nz@users.noreply.github.com>
2019-11-24 16:49:44 +1300

    Update glog alias

--- git/aliases.3.zsh
+++ git/aliases.3.zsh
@@ -1,7 +1,6 @@
 # Must run after zgen unaliases gdt
 
 # holman's git aliases
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
@@ -12,6 +11,7 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 
 # Mine
+alias glog="git log --graph --format='%C(auto)%h %s%d %C(dim yellow)(%C(blue)%an%C(yellow), %C(green)%cr%C(yellow))%Creset'"
 alias gdt='git difftool'
 alias gpf='git push --force-with-lease'
 alias gacpf='gaa && gc --amend --no-edit && gpf'
