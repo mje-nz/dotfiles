ad4e9da9fabf2b40ea9e049d2d878853e5ede81e
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-06-22 12:58:47 +1200

    Add --init to docker run aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,4 +1,4 @@
 alias db='docker build'
 alias dr='docker run'
-alias dri='docker run --rm -it'
-alias drx='docker run --rm -it -v "/tmp/.X11-unix:/tmp/.X11-unix" -e DISPLAY=$DISPLAY'
+alias dri='docker run --rm --interactive --tty --init'
+alias drx='dri -v "/tmp/.X11-unix:/tmp/.X11-unix" -e DISPLAY=$DISPLAY'
