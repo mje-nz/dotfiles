ef3e0eec2a3011e84f3633e70b8309e9b3a6508f
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-06-23 22:25:07 +1200

    Add dris alias

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,4 +1,5 @@
 alias db='docker build'
 alias dr='docker run'
 alias dri='docker run --rm --interactive --tty --init'
-alias drx='dri -v "/tmp/.X11-unix:/tmp/.X11-unix" -e DISPLAY=$DISPLAY'
+alias dris='dri --volume "$(pwd):/src" --workdir /src'
+alias drx='dri --volume "/tmp/.X11-unix:/tmp/.X11-unix" --env DISPLAY=$DISPLAY'
