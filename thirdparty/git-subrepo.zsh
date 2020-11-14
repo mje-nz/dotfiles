b6f67aa725dce1a8bb17c17db6dbb4883c6fac4a
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-11-14 14:56:32 +1300

    Fix git-subrepo

--- thirdparty/git-subrepo.zsh
+++ thirdparty/git-subrepo.zsh
@@ -1,5 +1,8 @@
-# Load git-subrepo. 
-source $DOTFILES/thirdparty/git-subrepo/.rc
+# Enable git-subrepo
 
-# TODO: Figure out why completions don't work.
-# 
+# Currently (2020-11-14), the bundled rc file breaks zsh with something related
+# to https://github.com/ingydotnet/git-subrepo/issues/183
+export GIT_SUBREPO_ROOT=$DOTFILES/thirdparty/git-subrepo
+export PATH=$GIT_SUBREPO_ROOT/lib:$PATH
+export MANPATH=$GIT_SUBREPO_ROOT/man:$MANPATH
+fpath=($GIT_SUBREPO_ROOT/share/zsh-completion $fpath)
