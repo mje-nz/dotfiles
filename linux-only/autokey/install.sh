af5848eb610da209d8d6525eefda77fdbdb60365
Matthew Edwards <mje-nz@users.noreply.github.com>
2020-11-15 12:57:34 +1300

    Remove hack from setup that used to fix skip/overwrite/backup all behaviour

--- linux-only/autokey/install.sh
+++ linux-only/autokey/install.sh
@@ -5,13 +5,5 @@ set -e
 # shellcheck disable=SC1090
 source "$DOTFILES/setup_common.sh"
 
-
-link_scripts() {
-	# TODO is this right?
-	# shellcheck disable=SC2034
-	local overwrite_all=false backup_all=false skip_all=false
-	mkdir -p ~/.config/autokey/data
-	link_file "$DOTFILES/linux-only/autokey/osxkeys/" "$HOME/.config/autokey/data/OSXKeys"
-}
-
-link_scripts
+mkdir -p ~/.config/autokey/data
+link_file "$DOTFILES/linux-only/autokey/osxkeys/" "$HOME/.config/autokey/data/OSXKeys"
--- ssh/install.sh
+++ ssh/install.sh
@@ -5,12 +5,5 @@ set -e
 # shellcheck disable=SC1090
 source "$DOTFILES/setup_common.sh"
 
-link_config() {
-	# TODO is this right?
-	# shellcheck disable=SC2034
-	local overwrite_all=false backup_all=false skip_all=false
-	mkdir -p ~/.ssh
-	link_file "$DOTFILES/ssh/config" "$HOME/.ssh/config"
-}
-
-link_config
+mkdir -p ~/.ssh
+link_file "$DOTFILES/ssh/config" "$HOME/.ssh/config"
