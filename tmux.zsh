b76f4316cdeba14d7b445db783680f3e28e4c49c
Matthew Edwards <mje-nz@users.noreply.github.com>
2022-07-04 12:15:18 +1200

    Automatically update env in tmux sessions

---
+++ tmux.zsh
@@ -0,0 +1,12 @@
+# Auto-update SSH_AUTH_SOCK etc when in a tmux session
+# See https://www.babushk.in/posts/renew-environment-tmux.html
+if [ -n "$TMUX" ]; then
+  function tmux_update_env {
+	eval $(tmux show-environment -s)
+  }
+else
+  function tmux_update_env { }
+fi
+
+autoload -Uz  add-zsh-hook
+add-zsh-hook preexec tmux_update_env
