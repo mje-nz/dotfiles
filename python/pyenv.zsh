5fa710fafc2f0ee5a6079f39d389f5b6d253aae5
Matthew Edwards <mje-nz@users.noreply.github.com>
2024-01-19 02:56:10 +0000

    Support auto-installed pyenv

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 # (shebang is only for shellcheck)
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PATH:$PYENV_ROOT/bin"
 if type pyenv &> /dev/null; then
 	eval "$(pyenv init -)"
 fi
